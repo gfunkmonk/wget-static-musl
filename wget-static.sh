9f92d5327cbabdc42d45985b71c9bc4a6ec6c243
Matt Rinsch <gfunkmonk@mailbox.org>
2026-03-07 03:12:00 -0500

    upd

--- wget-static.sh
+++ wget-static.sh
@@ -1,21 +1,71 @@
 #!/bin/bash
+set -euo pipefail
 
-##install some dependencies
-sudo apt -y install wget curl strip
+ORANGE="\033[38;2;255;165;0m"
+LEMON="\033[38;2;255;244;79m"
+TAWNY="\033[38;2;204;78;0m"
+HELIOTROPE="\033[38;2;223;115;255m"
+VIOLET="\033[38;2;143;0;255m"
+MINT="\033[38;2;152;255;152m"
+AQUA="\033[38;2;18;254;202m"
+TOMATO="\033[38;2;255;99;71m"
+NC="\033[0m"
 
-##download alpine rootfs
-wget -c "https://dl-cdn.alpinelinux.org/alpine/v3.22/releases/x86_64/alpine-minirootfs-3.22.2-x86_64.tar.gz"
+ARCH=${ARCH:-x86_64}
 
-##extract rootfs
+##map arch to Alpine minirootfs URL and QEMU binary name
+case "${ARCH}" in
+  x86_64)
+    ALPINE_URL="https://dl-cdn.alpinelinux.org/alpine/v3.23/releases/x86_64/alpine-minirootfs-3.23.3-x86_64.tar.gz"
+    QEMU_ARCH=""
+    ;;
+  x86)
+    ALPINE_URL="https://dl-cdn.alpinelinux.org/alpine/v3.23/releases/x86/alpine-minirootfs-3.23.3-x86.tar.gz"
+    QEMU_ARCH="i386"
+    ;;
+  aarch64)
+    ALPINE_URL="https://dl-cdn.alpinelinux.org/alpine/v3.23/releases/aarch64/alpine-minirootfs-3.23.3-aarch64.tar.gz"
+    QEMU_ARCH="aarch64"
+    ;;
+  armhf)
+    ALPINE_URL="https://dl-cdn.alpinelinux.org/alpine/v3.23/releases/armhf/alpine-minirootfs-3.23.3-armhf.tar.gz"
+    QEMU_ARCH="arm"
+    ;;
+  armv7)
+    ALPINE_URL="https://dl-cdn.alpinelinux.org/alpine/v3.23/releases/armv7/alpine-minirootfs-3.23.3-armv7.tar.gz"
+    QEMU_ARCH="arm"
+    ;;
+  *)
+    echo "Unknown architecture: ${ARCH}"
+    exit 1
+    ;;
+esac
+
+TARBALL="${ALPINE_URL##*/}"
+
+echo -e "${AQUA}= install some dependencies${NC}"
+sudo apt update -qy && sudo apt -y install wget curl strip
+
+echo -e "${HELIOTROPE}= download alpine rootfs${NC}"
+wget -c "${ALPINE_URL}"
+
+echo -e "${MINT}= extract rootfs${NC}"
 mkdir pasta
-tar xf alpine-minirootfs-3.22.2-x86_64.tar.gz -C pasta/
+tar xf "${TARBALL}" -C pasta/
 
-##copy resolv.conf into the folder
+echo -e "${TOMATO}= copy resolv.conf into the folder${NC}"
 cp /etc/resolv.conf ./pasta/etc/
 
-##if fails in cat command add inside chroot line this command "cat src/css_.c >> src/css.c"
+echo -e "${TAWNY}= setup QEMU for cross-arch builds${NC}"
+if [ -n "${QEMU_ARCH}" ]; then
+  sudo apt update -qy && sudo apt -y install qemu-user-static
+  sudo mkdir -p ./pasta/usr/bin/
+  sudo cp "/usr/bin/qemu-${QEMU_ARCH}-static" "./pasta/usr/bin/"
+fi
 
-##mount, bind and chroot into dir
+echo -e "${ORANGE}= if fails in cat command add inside chroot line this command 'cat src/css_.c >> src/css.c'${NC}"
+
+echo -e "${VIOLET}= mount, bind and chroot into dir${NC}"
 sudo mount -t proc none ./pasta/proc/
 sudo mount --rbind /dev ./pasta/dev/
 sudo mount --rbind /sys ./pasta/sys/
@@ -37,13 +87,15 @@ flex \
 bison \
 libunistring-dev \
 libunistring-static \
-perl && curl -C - -L -O 'https://mirror.marwan.ma/gnu/wget/wget-1.25.0.tar.gz'
-tar xf wget-1.25.0.tar.gz
-cd wget-1.25.0/
-./configure CC=gcc --with-ssl=openssl LDFLAGS='-static -lidn2 -lunistring' PERL=/usr/bin/perl
-make
-exit"
+upx \
+perl && curl -L -O 'https://mirrors.ibiblio.org/gnu/wget/wget-1.25.0.tar.gz' && \
+tar xf wget-1.25.0.tar.gz && \
+cd wget-1.25.0/ && \
+./configure CC=gcc --with-ssl=openssl LDFLAGS='-static -lidn2 -lunistring' CFLAGS='-O3 -Wno-unterminated-string-initialization' PERL=/usr/bin/perl && \
+make -j\$(nproc) && \
+strip wget && \
+upx --ultra-brute wget"
 mkdir -p dist
-cp ./pasta/wget-1.25.0/src/wget dist/
-strip dist/wget
-tar -C dist -cJf dist/wget.tar.xz wget
+cp "./pasta/wget-1.25.0/src/wget" "dist/axel-${ARCH}"
+tar -C dist -cJf "dist/wget-${ARCH}.tar.xz" "wget-${ARCH}"
+echo -e "${LEMON}= All done!${NC}"
